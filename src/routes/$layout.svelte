3eba63bab3116c50ecad4e7f0148f2014008dfce
SureDroid <support@suredroid.com>
2021-03-07 04:41:40 -0600

    add files

---
+++ src/routes/$layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import "../global.css";
+
+	import {Cool} from "../test";
+
+	console.log(Cool)
+
+	import { MaterialApp } from 'svelte-materialify/src';
+</script>
+
+<!-- <MaterialApp> -->
+	<slot />
+<!-- </MaterialApp> -->
